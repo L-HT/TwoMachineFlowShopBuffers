8d65a5ea9baef02ab99d5a0067e728244b15f55b
L-HT <lht@informatik.uni-leipzig.de>
2019-02-04 14:44:40 +0100

    Add parameters file for DABC and HVNS.

---
+++ tests/parameters.R
@@ -0,0 +1,17 @@
+# The resulting parameters from irace.
+
+####################################
+# HVNS
+# nIter = 135456 (possible: [1000, 250000])
+# endTemperatureFactor = 0.876 (possible: [0.010, 0.990])
+####################################
+
+####################################
+# DABC
+# populationSize = 7 (possible: [1,100])
+# perturbationStrength = 7 (possible: [1,10])
+# destructionSize1 = 7 (possible: [1,10])
+# destructionSize2 = 4 (possible: [1,10])
+# numberOfInserts = 52 (possible: [1,100])
+# localSearchAttempts = 51 (possible: [1,100])
+####################################
